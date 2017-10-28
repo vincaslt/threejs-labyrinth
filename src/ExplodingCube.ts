a13adbc6d449c3f744033335485c2a1c6c371b33
Vincas Stonys <vincas.stonys@gmail.com>
2017-10-28 14:41:57 +0300

    Generate independent shape

--- src/ExplodingCube.ts
+++ src/ExplodingCube.ts
@@ -2,17 +2,45 @@ import * as THREE from 'three'
 
 export class ExplodingCube {
   object: THREE.Mesh
+  exploded: boolean
+  triangles: THREE.Mesh[]
 
   constructor(color: number = 0xffee00) {
-    const material = new THREE.MeshLambertMaterial({ color, side: THREE.DoubleSide, wireframe: true })
+    const material = new THREE.MeshLambertMaterial({ color, side: THREE.DoubleSide })
     const geom = new THREE.DodecahedronGeometry(1, 2)
     geom.computeBoundingBox()
-    // geom.vertices = geom.vertices.map(v => new THREE.Vector3(
-    //   v.x + Math.random(),
-    //   v.y + Math.random(),
-    //   v.z + Math.random()
-    // ))
     this.object = new THREE.Mesh(geom, material)
+  }
+
+  private generateTriangles(scene: THREE.Scene) {
+    const pos = this.object.position.clone()
+    const faces = (this.object.geometry as THREE.Geometry).faces.slice()
+    this.triangles = faces.map((face) => {
+      const geom = new THREE.Geometry()
+      geom.vertices.push(
+        face.vertexNormals[0],
+        face.vertexNormals[1],
+        face.vertexNormals[2]
+      )
+      geom.faces.push(new THREE.Face3(0, 1, 2))
+      var mesh = new THREE.Mesh(geom, new THREE.MeshLambertMaterial({ color: 0xff0000 }))
+      mesh.position.copy(pos)
+      return mesh
+    })
+    scene.add(...this.triangles)
+  }
+
+  public explode(scene: THREE.Scene) {
+    if (!this.exploded) {
+      this.exploded = true
+      scene.remove(this.object)
+      this.generateTriangles(scene)
+    }
+  }
+
+  public render() {
+    if (this.exploded) {
+    }
   }
 
   public getObject() {
--- src/MainGame.ts
+++ src/MainGame.ts
@@ -10,15 +10,14 @@ import { MeshWallGenerator } from './MeshWallGenerator'
 export class MainGame extends BasicGame {
   movement: MovementManager
   walls: THREE.Mesh[]
+  ddchd: ExplodingCube
   wallLines: Line[]
 
   init() {
     this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000)
     this.camera.position.x = 170
     this.camera.position.y = 7
-    // this.camera.position.y = 30
     this.camera.position.z = 320
-    // this.camera.rotateX(-0.5)
     this.movement = new MovementManager(this.camera, this.scene)
     this.buildScene()
   }
@@ -27,10 +26,11 @@ export class MainGame extends BasicGame {
     const mazeLines = getMaze(2, 1)
     this.wallLines = mazeLines
 
-    const ddchd = new ExplodingCube().getObject()
-    ddchd.position.x = 170
-    ddchd.position.y = 7
-    ddchd.position.z = 315
+    this.ddchd = new ExplodingCube()
+    const ddchdObj = this.ddchd.getObject()
+    ddchdObj.position.x = 170
+    ddchdObj.position.y = 7
+    ddchdObj.position.z = 315
 
     const wallGenerator = new MeshWallGenerator(mazeLines, 15)
     const ambientLight = new THREE.AmbientLight(0x0c0c0c, 20)
@@ -40,11 +40,16 @@ export class MainGame extends BasicGame {
     this.walls = wallGenerator.generateWalls()
     const lights = LightGenerator.generateLights()
 
-    this.scene.add(...this.walls, ceiling, floor, ambientLight, ...lights, ddchd)
+    this.scene.add(...this.walls, ceiling, floor, ambientLight, ...lights, ddchdObj)
   }
 
   render() {
     this.movement.render(this.walls, this.wallLines, 5)
+    const ddchdObj = this.ddchd.getObject()
+    if (this.camera.position.distanceTo(ddchdObj.position) < 5) {
+      this.ddchd.explode(this.scene)
+    }
+    this.ddchd.render()
   }
 
   update() {}
